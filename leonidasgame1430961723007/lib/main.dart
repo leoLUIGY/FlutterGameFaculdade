0b166dbb4b40deb6dc5c0965d588389caf5a0063
leonidas <leonidas200999@hotmail.com>
2020-05-19 16:21:18 -0300

    projeto da falculdade para jogo com flutter parte 2

--- leonidasgame1430961723007/lib/main.dart
+++ leonidasgame1430961723007/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/time.dart';
 
 void main()
 {
   runApp(GameWidget());
 }
 Paint _white = Paint()..color= Color(0xFFFFFFFF);
+
 class GameObject{
   Rect position;
   void render (Canvas canvas)
@@ -33,10 +35,20 @@ class GameWidget extends StatelessWidget
 }
 
 class SpaceShooterGame extends Game{
+  static const enemy_speed = 400;
+  Timer enemyCreator;
+  List<GameObject> enemies = [];
+
   GameObject player;
   SpaceShooterGame()
   {
     player = GameObject()..position = Rect.fromLTWH(100, 100, 50, 50);
+    enemyCreator = Timer(1.0, repeat:true, callback:(){
+      enemies.add(
+        GameObject()..position = Rect.fromLTWH(0, 0, 50, 50)
+      );
+    });
+    enemyCreator.start();
   }
 
   void onPlayerMove(Offset delta)
@@ -45,11 +57,17 @@ class SpaceShooterGame extends Game{
   }
   void update(double dt)
   {
-
+    enemyCreator.update(dt);
+  enemies.forEach((enemy){
+    enemy.position = enemy.position.translate(0, enemy_speed * dt);
+  });
   }
 
   void render(Canvas canvas)
   {
   player.render(canvas);
+  enemies.forEach((enemy){
+    enemy.render(canvas);
+  });
   }
 }
